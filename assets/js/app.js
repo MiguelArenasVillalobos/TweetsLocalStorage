9d6413cb0641154f73915eaf3f3629494edfe61f
Miguel Arenas Villalobos <admin@miguelarenasvillalobos.com>
2020-03-03 12:00:19 -0600

    Proyecto Listo

---
+++ assets/js/app.js
@@ -0,0 +1,124 @@
+//Variables
+const listaTweets = document.getElementById('lista-tweets');
+
+
+
+
+// Eventos Listeners
+
+eventListeners();
+
+function eventListeners() {
+  // Cuando se envia al formulario
+  document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+
+  // Borrar Tweets
+  listaTweets.addEventListener('click', borrarTweet);
+
+  // Contenido cargado
+  document.addEventListener('DOMContentLoaded', localStorageListo);
+}
+
+
+
+
+
+// Funciones
+
+function agregarTweet(e) {
+  e.preventDefault();
+  // Leer el valor de textarea
+  const tweet = document.getElementById('tweet').value;
+
+  // Crear boton de eliminar
+  const botonBorrar = document.createElement('a');
+  botonBorrar.classList = 'borrar-tweet';
+  botonBorrar.innerText = 'X';
+
+  // Crear elemento y añadirlo a la lista
+  const li = document.createElement('li');
+  li.innerText = tweet;
+  // Añade boton borrar
+  li.appendChild(botonBorrar);
+  // Añado el tweet a la lista
+  listaTweets.appendChild(li);
+
+  // Añadir a Local Storage
+  agregarTweetLocalStorage(tweet);
+} 
+
+// Borrar tweet
+function borrarTweet(e) {
+  e.preventDefault();
+
+  if(e.target.className === 'borrar-tweet') {
+    e.target.parentElement.remove();
+    borrarTweetLocalStorage(e.target.parentElement.innerText);
+  }
+}
+
+// Mostrar datos de local storage en la lista
+function localStorageListo() {
+  let tweets;
+
+  tweets = obtenerTweetsLocalStorage();
+
+  tweets.forEach(function(tweet) {
+    // Crear boton de eliminar
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'borrar-tweet';
+    botonBorrar.innerText = 'X';
+
+    // Crear elemento y añadirlo a la lista
+    const li = document.createElement('li');
+    li.innerText = tweet;
+    // Añade boton borrar
+    li.appendChild(botonBorrar);
+    // Añado el tweet a la lista
+    listaTweets.appendChild(li);
+  });
+}
+
+// Agrega tweet a local storage
+function agregarTweetLocalStorage(tweet) {
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+
+  // Añadir el nuevo tweet
+  tweets.push(tweet);
+
+  // Convertir de String a local storage
+  localStorage.setItem('tweets', JSON.stringify(tweets) );
+}
+
+// Comprobar que haya elementos local storage y retorna un arreglo
+function obtenerTweetsLocalStorage() {
+  let tweets;
+  // Revisamos los valores de local storage
+  if(localStorage.getItem('tweets') === null) {
+    tweets = [];
+  } else {
+    tweets = JSON.parse(localStorage.getItem('tweets') );
+  }
+  return tweets;
+}
+
+// Eliminar tweet de local storage
+function borrarTweetLocalStorage(tweet) {
+  
+  let tweets, tweetBorrar;
+  // Eliminar la X del tweet
+  tweetBorrar = tweet.substring(0, tweet.length - 1);
+
+  tweets = obtenerTweetsLocalStorage();
+
+  tweets.forEach(function(tweet, index) {
+    if(tweetBorrar == tweet) {
+      tweets.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tweets', JSON.stringify(tweets) );
+
+  console.log(tweets);
+}
